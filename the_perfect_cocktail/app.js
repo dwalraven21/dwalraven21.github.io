05000db9ab7d77b19c73956d70f602316b0b8606
Dany Walraven <dwalraven21@gmail.com>
2019-07-21 17:24:15 -0400

    updated favorite drink section that utilizes local storage

--- the_perfect_cocktail/app.js
+++ the_perfect_cocktail/app.js
@@ -144,7 +144,7 @@ $(document).on('click', '#save', () => {
 
 });
 
-if (localStorage.getItem('favDrinkName') !== 'null') {
+if (localStorage.getItem('favDrinkName') !== null) {
 	$favoriteName = localStorage.getItem('favDrinkName')
 	$favoriteRecipe = localStorage.getItem('favDrinkRecipe')
 	$favoriteImage = localStorage.getItem('favDrinkPic')
